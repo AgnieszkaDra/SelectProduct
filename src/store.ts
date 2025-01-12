4999cc3345d3be819af8c61b2003e673a62a70f2
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-01-12 20:39:32 +0100

    some changes in order to deploy on netlify

--- src/store.ts
+++ src/store.ts
@@ -5,7 +5,8 @@ export const store = configureStore({
     reducer: {
         products: productsReducer,
     },
+    devTools: process.env.NODE_ENV !== 'production'
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
